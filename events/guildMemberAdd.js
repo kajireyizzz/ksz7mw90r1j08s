65b89e151b6b4fc2e930f3a6d0a3941adf4d7762
kajireyizzz <39592492+kajireyizzz@users.noreply.github.com>
2018-05-29 23:37:08 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,28 @@
+const ayarlar = require('../ayarlar.json');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+  
+
+
+
 module.exports = member => {
-  const channel = member.guild.channels.find('name', 'giriş-çıkış');
-  if (!channel) return;
-  channel.send(`:inbox_tray: **|${member} sunucuya katıldı.**`);
+    let username = member.user.username;
+    let guild = member.guild;
+    
+    const channel = member.guild.channels.find('name', 'logs');
+    if (!channel) return;
+    const embed = new Discord.RichEmbed()
+    .setColor('#4cff00')
+    
+    .setAuthor("Yeni Üye.!", member.user.avatarURL || member.user.defaultAvatarURL)
+    .setTitle(`@${member.user.username} #${member.user.discriminator} Sunucuya katıldı,${member.guild.memberCount} .Üye!.`)
+    .setThumbnail(member.user.avatarURL || member.user.defaultAvatarURL)
+   
+  
+    
+    .setTimestamp()
+    .setFooter("giris-cikis sistemi>>Penia BOT")
+    channel.send(embed);
 };
