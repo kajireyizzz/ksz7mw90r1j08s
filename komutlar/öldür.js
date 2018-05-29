6983fcf730e77e9d1cc899d2dd6ba407a61d9625
kajireyizzz <39592492+kajireyizzz@users.noreply.github.com>
2018-05-29 23:22:34 +0300

    Update öldür.js

--- komutlar/öldür.js
+++ komutlar/öldür.js
@@ -3,14 +3,14 @@ const Discord = require('discord.js');
 exports.run = function(client, message, args) {
  let user = message.mentions.users.first();
    
-      let user = message.mentions.users.first();
+    
     if (message.mentions.users.size < 1) return message.reply('Kim için bu komutu İstediğini Yazmalısın `Örn Kullanım: p!öldür @PeniaBOT`').catch(console.error);
  
     const oldur=new Discord.RichEmbed()
     .setColor("RANDOM")
     .addField(`${message.author.username}`,`${user}`+ " kişisini öldürdü.!")
     .setImage('https://cdn.discordapp.com/attachments/363746758083477505/400337232625401856/animation_2.gif')
-    .setFooter("OA Premium")
+    .setFooter("Penia BOT")
     return message.channel.send(oldur);
 
 
